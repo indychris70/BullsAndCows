98b0822ab1d647bda4e36608cbee4bf362ce3bd1
indychris70 <chrisjpierce@gmail.com>
2021-06-09 16:29:47 -0400

    Stage #4: Game time!

--- Grader.java
+++ Grader.java
@@ -2,27 +2,34 @@ package bullscows;
 
 public class Grader {
     private final String code;
-    private final String guess;
     private int cows;
     private int bulls;
     private String result;
 
-    Grader(String guess, String code) {
+    Grader(String code) {
         this.code = code;
-        this.guess = guess;
-        gradeGuess();
-        buildResult();
+    }
+
+    public boolean gameOver() {
+        return bulls == code.length();
     }
 
     public void printResult() {
         System.out.println(result);
     }
 
-    private void gradeGuess() {
+    public void submitGuess(String guess) {
+        reset();
         for (int i = 0; i < guess.length(); i++) {
             cows = isCow(i, guess.charAt(i)) ? cows + 1 : cows;
             bulls = isBull(i, guess.charAt(i)) ? bulls + 1 : bulls;
         }
+        buildResult();
+    }
+
+    private void reset() {
+        cows = 0;
+        bulls = 0;
     }
 
     private boolean isCow(int index, char c) {
@@ -40,12 +47,12 @@ public class Grader {
 
     private void buildResult() {
         StringBuilder statement = new StringBuilder("Grade: ");
-        statement.append(hasCows() ? String.format("%d cows(s)", cows) : "");
-        statement.append(hasCows() && hasBulls() ? " and " : ".");
+        statement.append(hasCows() ? String.format("%d cow(s)", cows) : "");
+        statement.append(hasCows() && hasBulls() ? " and " : "");
         statement.append(hasBulls() ? String.format("%d bull(s)", bulls) : "");
         statement.append(!hasCows() && !hasBulls() ? "None" : "");
         statement.append(".");
-        result = statement.append(String.format("The secret code is %s.", code)).toString();
+        result = statement.toString(); // .append(String.format("The secret code is %s.", code)).toString();
     }
 
     private boolean hasCows() {
--- Main.java
+++ Main.java
@@ -3,14 +3,40 @@ package bullscows;
 import java.util.Scanner;
 
 public class Main {
+    private enum Messages {
+        START_GAME("Okay, let's start a game!"),
+        GAME_WON("Congratulations! You guessed the secret code."),
+        TURN_NUMBER("Turn %s:"),
+        ERROR("Error");
+
+        String text;
+
+        Messages(String text) {
+            this.text = text;
+        }
+
+        public void print(String... strings) {
+            System.out.println(String.format(text, strings));
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int codeLength = scanner.nextInt();
-        String message = "Error";
         if (codeLength <= 10) {
             Code secretCode = new Code(codeLength);
-            message = String.format("The random secret number is %s.", secretCode.getValue());
+            Grader grader = new Grader(secretCode.getValue());
+            int turn = 1;
+
+            Messages.START_GAME.print();
+            do {
+                Messages.TURN_NUMBER.print(Integer.toString(turn));
+                grader.submitGuess(scanner.nextLine());
+                grader.printResult();
+                turn++;
+            } while (!grader.gameOver());
+            Messages.GAME_WON.print();
+        } else {
+            Messages.ERROR.print();
         }
-        System.out.println(message);
     }
 }
