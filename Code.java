54add61c7d963ff6558a697db8e5a91bb759e5f4
indychris70 <chrisjpierce@gmail.com>
2021-06-10 22:19:45 -0400

    Stage #6: New level

--- Code.java
+++ Code.java
@@ -1,26 +1,61 @@
 package bullscows;
 
 import java.util.Collections;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class Code {
     private String value;
+    private String description;
+    private String validCodeCharacters = "0123456789abcdefghijklmnopqrstuvwxyz";
 
-    Code(int requiredLength) {
-        value = generateCode(requiredLength);
+    Code(int requiredLength, int requiredNumberOfCharacters) {
+        value = generateCode(requiredLength, requiredNumberOfCharacters);
+        description = generateCodeDescription(requiredLength, requiredNumberOfCharacters);
     }
 
     public String getValue() {
         return value;
     }
 
-    private String generateCode(int requiredLength) {
-        List<Character> coll = Arrays.asList('0', '1', '2', '3', '4', '5', '6', '7', '8','9');
-        Collections.shuffle(coll);
-        StringBuilder code = new StringBuilder();
+    public String getSecretDescription() {
+        return description;
+    }
+
+    private String generateCodeDescription(int requiredLength, int requiredNumberOfCharacters) {
+        String maskedCode = generateMaskedCode(requiredLength);
+        String includedCharacters = getIncludedCharacters(requiredNumberOfCharacters);
+        return String.format("%s %s.", maskedCode, includedCharacters);
+    }
+
+    private String generateMaskedCode(int requiredLength) {
+        StringBuilder maskedCode = new StringBuilder(requiredLength);
         for (int i = 0; i < requiredLength; i++) {
-            code.append(coll.get(i));
+            maskedCode.append("*");
+        }
+        return maskedCode.toString();
+    }
+
+    private String getIncludedCharacters(int requiredNumberOfCharacters) {
+        StringBuilder includedCharacters = new StringBuilder();
+        if (requiredNumberOfCharacters <= 10) {
+            includedCharacters.append(String.format("(0-%d)", requiredNumberOfCharacters - 1));
+        } else {
+            includedCharacters.append(String.format("(0-9, a-%s)", validCodeCharacters.charAt(requiredNumberOfCharacters - 1)));
+        }
+        return includedCharacters.toString();
+    }
+
+    private String generateCode(int requiredCodeLength, int requiredNumberOfCharacters) {
+        List<Character> charactersToUse = new ArrayList(requiredNumberOfCharacters);
+        for (int i = 0; i < requiredCodeLength; i++) {
+            charactersToUse.add(validCodeCharacters.charAt(i));
+        }
+        Collections.shuffle(charactersToUse);
+        StringBuilder code = new StringBuilder(charactersToUse.size());
+        for (char c : charactersToUse) {
+            code.append(c);
         }
         return code.toString();
     }
--- Main.java
+++ Main.java
@@ -7,7 +7,10 @@ public class Main {
         START_GAME("Hello Professor. Would you like to play a game?"),
         GAME_WON("Congratulations! You guessed the secret code."),
         TURN_NUMBER("Turn %s:"),
-        ERROR("Error");
+        ERROR("Error"),
+        PROMPT_CODE_LENGTH("Input the length of the secret code:"),
+        PROMPT_NUMBER_SYMBOLS("Input the number of possible symbols in the code:"),
+        SECRET_READY("The secret is prepared: %s");
 
         String text;
 
@@ -20,10 +23,17 @@ public class Main {
         }
     }
     public static void main(String[] args) {
+        final int MAX_CODE_LENGTH = 36;
         Scanner scanner = new Scanner(System.in);
+
+        Messages.PROMPT_CODE_LENGTH.print();
         int codeLength = scanner.nextInt();
-        if (codeLength <= 10) {
-            Code secretCode = new Code(codeLength);
+        if (codeLength <= MAX_CODE_LENGTH) {
+            Messages.PROMPT_NUMBER_SYMBOLS.print();
+            int numberOfSymbols = scanner.nextInt();
+            Code secretCode = new Code(codeLength, numberOfSymbols);
+            System.out.println(secretCode.getValue());
+            Messages.SECRET_READY.print(secretCode.getSecretDescription());
             Game game = new Game(secretCode.getValue());
             int turn = 1;
 
